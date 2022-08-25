bd694048a9da60f35c2967add5699201c76164e3
mingxiang-isao <mingxiang-isao>
2022-08-25 08:39:04 +0000

    test commit

--- project/notebooks/notebook_2.py
+++ project/notebooks/notebook_2.py
@@ -6,7 +6,16 @@
 
 from pyspark.sql import SparkSession
 
-
 def generate_data(table_name="test_table"):
     df = SparkSession.getActiveSession().range(0, 10)
     df.write.format("delta").mode("overwrite").saveAsTable(table_name)
+
+# COMMAND ----------
+
+# DBTITLE 1,Test SQL command
+# MAGIC %sql
+# MAGIC SELECT * FROM test_table
+
+# COMMAND ----------
+
+# MAGIC %fs ls /tmp
